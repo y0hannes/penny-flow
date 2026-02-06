a5564fbd09c6f52a6e51f5ea0804d74e12473baf
y0hannes <yohanneset3@gmail.com>
2026-02-06 15:33:32 +0300

    feat(screens): add screen scaffolding for navigation

---
+++ src/screens/Insights/InsightsScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function InsightsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Insights Screen </Text>
+    </View>
+  );
+}
